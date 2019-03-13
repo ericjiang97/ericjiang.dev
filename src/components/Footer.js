ec8d2cc474296a8225745014bf7a7ffc3bb0571a
Eric Jiang <ericjiang97@gmail.com>
2019-03-13 14:54:52 +1100

    cleaning up subscribe form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { css } from '@emotion/core'
 import { bpMaxSM } from '../lib/breakpoints'
-import SubscribeForm from './Forms/Subscribe'
 import { Twitter, GitHub } from './Social'
 import Container from './Container'
 
