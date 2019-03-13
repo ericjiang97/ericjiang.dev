92ac5af04a6c730d432f2d9795d69aaee71c2261
Eric Jiang <ericjiang97@gmail.com>
2019-03-14 08:17:33 +1100

    added about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,7 +21,38 @@ export default function Index({ data: { site, allMdx } }) {
         `}
       >
         <h1>About Me</h1>
-        <span>Info about me goes here</span>
+        <p>
+          I'm currently in my final year (studying part-time) in my Bachelor of
+          Information Technology at{' '}
+          <a
+            href="https://monash.edu"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Monash University
+          </a>{' '}
+          degree where I'm majoring in Computer networks and security. Even
+          though I'm majoring in the Cybersecurity, I still love other sides of
+          the IT field such as Product Management, Software Engineering and
+          Web/Cloud Technologies. And applying knowledge from the Cybersecurity
+          and DevOps into Software Development.
+        </p>
+        <p>
+          Over the past few years I've been building{' '}
+          <a
+            href="https://monplan.apps.monash.edu"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            MonPlan
+          </a>
+          , which is a project that I founded and that the University brought
+          on-board - which is now the Official Enterprise Course Planning tool
+          for the university. Something that I'm currently proud of is winning
+          the Rising Star Category of the 2019 itNews Benchmark Awards for
+          delivering MonPlan (while I'm still a student)
+        </p>
+        <p>You can find my resume here</p>
       </Container>
     </Layout>
   )
